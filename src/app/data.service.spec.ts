6dd07a5af9b111ecf817a1bfd0fdf3347d2e86a5
Luke Avedon <Luke.Avedon@coniferhealth.com>
2023-06-23 15:18:11 -0400

    writing service tests

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -3,7 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './data.service';
 
-fdescribe('DataService', () => {
+describe('DataService', () => {
   let service: DataService;
   let httpMock: HttpTestingController;
 
@@ -17,7 +17,26 @@ fdescribe('DataService', () => {
     httpMock = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch data from the API via GET', () => {
+    const mockData = [{ Id: 1, Name: 'John', Occupation: 'Engineer', Age: 30, Email: 'john@test.com' }];
+
+    service.getData(0).subscribe(data => {
+      expect(data.length).toBe(1);
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:5000/api/data/0');
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockData);
   });
 });
