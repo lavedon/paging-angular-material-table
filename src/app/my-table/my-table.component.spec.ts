3bba7e823d915a45895d1706be384270173eb66d
Luke Avedon <Luke.Avedon@coniferhealth.com>
2023-06-23 16:02:33 -0400

    basic table test

--- src/app/my-table/my-table.component.spec.ts
+++ src/app/my-table/my-table.component.spec.ts
@@ -2,20 +2,35 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MyTableComponent } from './my-table.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DataService } from '../data.service';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 describe('MyTableComponent', () => {
   let component: MyTableComponent;
   let fixture: ComponentFixture<MyTableComponent>;
+  let dataService: DataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ MyTableComponent ],
-      imports: [ HttpClientTestingModule ]
+      imports: [ MatPaginatorModule ],
+      providers: [
+        {
+          provide: DataService,
+          useValue: {
+            getData: () => of([{ Id: 1, Name: 'John', Occupation: 'Engineer', Age: 30, Email: 'johntheengineer@testemail.com' }])
+          }
+        }
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(MyTableComponent);
     component = fixture.componentInstance;
+    dataService = TestBed.inject(DataService);
     fixture.detectChanges();
   });
 
