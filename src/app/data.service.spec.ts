d95ff07d84bf8c8c9b964733d1c51648d2587a88
Luke Avedon <Luke.Avedon@coniferhealth.com>
2023-06-23 15:48:24 -0400

    small tests

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './data.service';
 
 describe('DataService', () => {
@@ -10,11 +9,12 @@ describe('DataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ],
-      providers: [ DataService, HttpTestingController ]
+      providers: [ DataService ]
     });
 
     service = TestBed.inject(DataService);
     httpMock = TestBed.inject(HttpTestingController);
+    console.log('httpMock: ', httpMock);
   });
 
   afterEach(() => {
@@ -38,5 +38,22 @@ describe('DataService', () => {
     expect(req.request.method).toBe('GET');
 
     req.flush(mockData);
+    httpMock.verify();
+  });
+  it('should fetch multiple records from the API via GET', () => {
+    const mockData = [{ Id: 1, Name: 'John', Occupation: 'Engineer', Age: 30, Email: 'john@test.com' },
+    { Id: 2, Name: 'Paul', Occupation: 'Doctor', Age: 49, Email: 'paul@test.com' }];
+
+    service.getData(0).subscribe(data => {
+      expect(data.length).toBe(2);
+      expect(data).toEqual(mockData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:5000/api/data/0');
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockData);
+    httpMock.verify();
   });
 });
