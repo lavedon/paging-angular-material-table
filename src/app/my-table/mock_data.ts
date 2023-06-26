946437a084464b12bfb83c74aab3bbe23314a289
Luke <lavedon@gmail.com>
2023-06-26 13:05:27 -0400

    setting up mock data

---
+++ src/app/my-table/mock_data.ts
@@ -0,0 +1,14 @@
+export const MOCK_DATA: Data[] = [
+  { Id: 1, Name: 'John Doe', Occupation: 'Developer', Age: 27, Email: 'john.doe@example.com' },
+  { Id: 2, Name: 'Jane Smith', Occupation: 'Designer', Age: 32, Email: 'jane.smith@example.com' },
+  { Id: 3, Name: 'Alice Johnson', Occupation: 'Architect', Age: 45, Email: 'alice.johnson@example.com' },
+  { Id: 4, Name: 'Bob Williams', Occupation: 'Engineer', Age: 34, Email: 'bob.williams@example.com' },
+  { Id: 5, Name: 'Charlie Brown', Occupation: 'Teacher', Age: 29, Email: 'charlie.brown@example.com' },
+  { Id: 6, Name: 'David Davis', Occupation: 'Police Officer', Age: 36, Email: 'david.davis@example.com' },
+  { Id: 7, Name: 'Ella Wilson', Occupation: 'Nurse', Age: 39, Email: 'ella.wilson@example.com' },
+  { Id: 8, Name: 'Frank Thomas', Occupation: 'Chef', Age: 41, Email: 'frank.thomas@example.com' },
+  { Id: 9, Name: 'Grace Martinez', Occupation: 'Doctor', Age: 45, Email: 'grace.martinez@example.com' },
+  { Id: 10, Name: 'Harry Anderson', Occupation: 'Lawyer', Age: 50, Email: 'harry.anderson@example.com' },
+  { Id: 11, Name: 'Ivy Taylor', Occupation: 'Artist', Age: 28, Email: 'ivy.taylor@example.com' },
+];
+
--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -1,38 +1,15 @@
-import { AfterViewInit, Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
-import { Data } from 'i-data-model';
-import { MatPaginator } from '@angular/material/paginator';
-import { catchError, Observable, of, switchMap, startWith } from 'rxjs';
+import { Component } from '@angular/core';
+import { Data } from '../types/i-data-model'
 
 @Component({
   selector: 'my-table',
   templateUrl: './my-table.component.html',
   styleUrls: ['./my-table.component.css']
 })
-export class MyTableComponent implements AfterViewInit, OnDestroy {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  displayedColumns: string[] = ['Id', 'Name', 'Occupation', 'Age', 'Email'];
-  dataSource$!: Observable<Data[]>;
+export class MyTableComponent {
+  displayedColumns: string[] = ['Name', 'Occupation', 'Age', 'Email']
 
-  constructor(private dataService: DataService) { }
 
-  ngAfterViewInit(): void {
-    this.paginator.pageIndex = 0;
-    this.dataSource$ = this.paginator.page.pipe(
-      startWith({}),
-      switchMap(() => {
-        return this.dataService.getData(this.paginator.pageIndex + 1);
-      }),
-      catchError(() => {
-        return of([]);
-      })
-    );
 
-    console.count('About to call next on paginator');
-    this.paginator.page.next({});
-  }
-
-  ngOnDestroy() {
-  }
 }
 
